4e6d96310b7069c82800c7403090e14a6983672a
vkowalski <vkowalski@lab-code.com>
2021-08-23 18:08:08 +0300

    api

--- app/src/main/java/com/victor/gbooks/data/Api.kt
+++ app/src/main/java/com/victor/gbooks/data/Api.kt
@@ -1,4 +1,50 @@
 package com.victor.gbooks.data
 
+import com.google.gson.GsonBuilder
+import com.victor.gbooks.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 interface Api {
+    companion object Factory {
+        //apikey = AIzaSyDV87G8uwwe3Tubm8u5dsMhJXuSTEGied0
+        //https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyDV87G8uwwe3Tubm8u5dsMhJXuSTEGied0
+
+        private const val BASE_API_URL = "https://www.googleapis.com/books/v1/volumes"
+        private const val QUERY_PARAMETER_KEY = "q"
+        private const val KEY = "key"
+        private const val API_KEY = "AIzaSyDV87G8uwwe3Tubm8u5dsMhJXuSTEGied0"
+        private const val TITLE = "intitle:"
+        private const val AUTHOR = "inauthor:"
+        private const val PUBLISHER = "inpublisher:"
+        private const val ISBN = "isbn:"
+
+        fun create(): Api {
+
+            val httpClient = OkHttpClient.Builder()
+
+            if (BuildConfig.DEBUG) {
+                val logging = HttpLoggingInterceptor()
+                logging.level = HttpLoggingInterceptor.Level.BODY
+
+                httpClient.addInterceptor(logging)
+            }
+
+            val gson = GsonBuilder()
+                    .setLenient()
+                    .create()
+
+            val retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_API_URL)
+                    .client(httpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+
+            return retrofit.create(Api::class.java)
+        }
+    }
 }
